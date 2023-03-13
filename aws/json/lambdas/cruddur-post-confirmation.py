4d3d6ed05a082117c65b6bd3aca85bfc272984d8
DavidTausend <davidtausend-911@hotmail.com>
2023-03-13 20:56:15 +0000

    Cognito Post Confirmation Lambda

--- aws/json/lambdas/cruddur-post-confirmation.py
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -1,32 +1,36 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
     user_display_name = user['name']
     user_email = user['email']
-    user_handel = user['preferred_username']
-    user_cognito_id= user['sub']
+    user_handle = user['preferred_username']
+    user_cognito_id = user['sub']
     try:
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
+      print('entered-try')
 
-        sql = f"""
-          "INSERT INTO users (
-            display_name,
-            email,
-            handle,
-            cognito_user_id
-            )
-          VALUES(
-            {user_display_name},
-            {user_email},
-            {user_handle},
-            {user_cognito_id}
-         )"
+      sql = f"""
+        INSERT INTO users (
+         display_name,
+         email,
+         handle,
+         cognito_user_id
+          )
+         VALUES(
+          '{user_display_name}',
+          '{user_email}',
+          '{user_handle}',
+          '{user_cognito_id}'
+         )
         """
-        cur.execute(sql)
-        conn.commit() 
+      print('SQL Statement  ----')
+      print(sql)
+      conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
