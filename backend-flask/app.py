51d6e45ba38349083011aed588b8e6813b0be8fd
DavidTausend <davidtausend-911@hotmail.com>
2023-03-19 06:45:09 +0000

    MFA implementation

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -16,6 +16,7 @@ from services.create_message import *
 from services.show_activity import *
 
 #Cognito
+import boto3
 from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 from flask import jsonify
 
@@ -183,17 +184,48 @@ def data_home():
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
-     # authenicatied request
-    app.logger.debug("authenticated")
-    app.logger.debug(claims)
-    app.logger.debug(claims['username'])
-    data = HomeActivities.run(cognito_user_id=claims['username'])
+     # Check if MFA is required
+    try:
+          response = cognito.associate_software_token(
+              AccessToken=access_token
+          )
+    except cognito.exceptions.NotAuthorizedException:
+          # MFA is not required
+          data = HomeActivities.run(cognito_user_id=claims['username'])
+          return data, 200
+
+          # MFA is required - send an SMS verification code
+          phone_number = response['SecretCodeDeliveryDetails']['Destination']
+          cognito.verify_software_token(
+          AccessToken=access_token,
+          UserCode=request.form['mfa_code'],
+          FriendlyDeviceName='device'
+      )
+
+          # Authenticate the user using Amazon Cognito
+          response = cognito.initiate_auth(
+          AuthFlow='USER_PASSWORD_AUTH',
+          AuthParameters={
+              'USERNAME': claims['username'],
+              'PASSWORD': request.form['password'],
+              'MFA_TOKEN': request.form['mfa_code']
+          },
+          ClientId='your_client_id',
+          Session=response['Session']
+      )
+
+          # Return a new JWT token that includes the MFA authentication factor
+          return jsonify({
+          'access_token': response['AuthenticationResult']['AccessToken'],
+          'expires_in': response['AuthenticationResult']['ExpiresIn']
+      }), 200
+
   except TokenVerifyError as e:
-    # unathenicated request
-    app.logger.debug(e)
-    app.logger.debug("unauthenticated")
-    data = HomeActivities.run()
-  return data, 200
+      # Invalid JWT token
+      app.logger.debug(e)
+      app.logger.debug("unauthenticated")
+      data = HomeActivities.run()
+      return data, 401
 
 @app.route("/api/activities/notifications", methods=['GET'])
 #xray
