525056371cf23e2fb65d1757d41721f892bf3bd8
DavidTausend <davidtausend-911@hotmail.com>
2023-03-26 07:53:28 +0000

    resolving problems

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -10,7 +10,7 @@ VALUES
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'davidtausend' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
