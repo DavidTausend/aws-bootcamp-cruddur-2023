4397b7e34a949322b1406d0635c34d07174704c0
DavidTausend <davidtausend-911@hotmail.com>
2023-03-26 07:23:02 +0000

    dynamodb done

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -4,6 +4,7 @@ VALUES
   ('Andrew Brown', 'andrew@example.co', 'andrewbrown' ,'MOCK'),
   ('David Tausend', 'davidtausend-911@hotmail.com', 'davidtausend' ,'MOCK'),
   ('Ruta Tausend', 'rutatausend@gmail.com', 'rutatausend' ,'MOCK'),
+  ('David Padilla', 'david.tausend@servisa-gruppe.de', 'davidpadilla' ,'MOCK'),
   ('Andrew Bayko', 'bayko@example.co', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
