ef48313e0ed813d0fad8ebf76546629501d7cb9c
DavidTausend <davidtausend-911@hotmail.com>
2023-04-16 19:03:56 +0000

    resolving issues and working on edit button

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,29 +2,29 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
 export default function ProfileHeading(props) {
-    const backgroundImage = 'url("https://assets.hallotausend.com/banners/banner.jpg")';
-    const styles = {
-        backgroundImage: backgroundImage,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center', 
+  const backgroundImage = 'url("https://assets.hallotausend.com/banners/banner.jpg")';
+  const styles = {
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   };
   return (
-    <div className='activity_feed_heading.profile_heading'>
-        <div className='title'>{props.profile.display_name}</div>
-        <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
-        <div class="banner" style={styles}>
-          <div className="avatar">
-            <img src="https://assets.hallotausend.com/avatars/AWS.png"></img>
-         </div>
-        </div>
-        <div class="info">
-          <div class='id'>
-            <div className="display_name">{props.profile.display_name}</div>
-            <div className="handle">@{props.profile.handle}</div>
-          </div>
-          <EditProfileButton setPopped={props.setPopped}/>
-        </div>
-        
-    </div>  
+  <div className='activity_feed_heading profile_heading'>
+    <div className='title'>{props.profile.display_name}</div>
+    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
+    <div class="banner" style={styles} >
+      <div className="avatar">
+        <img src="https://assets.hallotausend.com/avatars/AWS.jpg"></img>
+      </div>
+    </div>
+    <div class="info">
+      <div class='id'>
+        <div className="display_name">{props.profile.display_name}</div>
+        <div className="handle">@{props.profile.handle}</div>
+      </div>
+      <EditProfileButton setPopped={props.setPopped} />
+    </div>
+
+  </div>
   );
 }
--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -2,12 +2,12 @@ import './HomeFeedPage.css';
 import React from "react";
 //import { trace } from '@opentelemetry/api';
 
-import DesktopNavigation  from '../components/DesktopNavigation';
-import DesktopSidebar     from '../components/DesktopSidebar';
-import ActivityFeed from '../components/ActivityFeed';
-import ActivityForm from '../components/ActivityForm';
-import ReplyForm from '../components/ReplyForm';
-import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+import DesktopNavigation  from 'components/DesktopNavigation';
+import DesktopSidebar     from 'components/DesktopSidebar';
+import ActivityFeed from 'components/ActivityFeed';
+import ActivityForm from 'components/ActivityForm';
+import ReplyForm from 'components/ReplyForm';
+import {checkAuth, getAccessToken} from 'lib/CheckAuth';
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
