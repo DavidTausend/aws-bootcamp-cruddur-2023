f466602ea20165f49dcc0aee2876203de466845e
DavidTausend <davidtausend-911@hotmail.com>
2023-04-22 09:25:18 +0000

    trying to solve Undefined Error

--- aws/lambdas/hallotausend-upload-avatar/function.rb
+++ aws/lambdas/hallotausend-upload-avatar/function.rb
@@ -6,7 +6,7 @@ def handler(event:, context:)
   puts event
   #return cors headers for preflight check
   if event['routeKey'] == "OPTIONS /{PROXY+}"
-    puts ({step:'preflight', message: 'preflight CORS check'}.to_json)
+    puts({step:'preflight', message: 'preflight CORS check'}.to_json)
     {
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
@@ -17,16 +17,19 @@ def handler(event:, context:)
     }
   else
     token = event['headers']['authorization'].split(' ')[1] 
-    puts access_token
-    puts ({step:'presignedurl', access_token: token}.to_json
+    puts({step:'presignedurl', access_token: token}.to_json)
+
+    body_hash = JSON.parse(event["body"])
+    extension = body_hash["extension"]
 
     decoded_token = JWT.decode token, nil, false
-    puts "decoded token"
-    puts decoded_token.inspect
+    cognito_user_uuid = decoded_token[0]['sub']
 
     s3 = Aws::S3::Resource.new
     bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-    object_key = 'mock.jpg'
+    object_key = "#{cognito_user_uuid}.#{extension}"
+
+    puts({"object_key": object_key“}.to_json)
 
     obj = s3.bucket(bucket_name).object(object_key)
     url = obj.presigned_url(:put, expires_in: 60 * 5)
@@ -38,7 +41,8 @@ def handler(event:, context:)
         "Access-Control-Allow-Origin": "https://3000-davidtausen-awsbootcamp-z5jbe015wb7.ws-eu95.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
-      statusCode: 200
+      statusCode: 200,
+      body: body
     }
   end
 end
--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -2,26 +2,21 @@ import './ProfileForm.css';
 import React from "react";
 import process from 'process';
 import {getAccessToken} from 'lib/CheckAuth';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-web';
-import { json } from 'react-router-dom';
 
 
 export default function ProfileForm(props) {
-  const [presignedurl, setPresignedurl] = React.useState('');
   const [bio, setBio] = React.useState('');
   const [displayName, setDisplayName] = React.useState('');
 
   React.useEffect(()=>{
-    if (props.profile.bio === null)
-      setBio('');
-    setBio(props.profile.bio);
+    setBio(props.profile.bio || '');
     setDisplayName(props.profile.display_name);
   }, [props.profile])
 
   const s3uploadkey = async (extension)=> {
     console.log('ext',extension)
     try {
-      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_UR}/avatars/key_upload`
+      const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
       const json = {
@@ -51,15 +46,14 @@ export default function ProfileForm(props) {
   const s3upload = async (event)=> {
     console.log('event',event)
     const file = event.target.files[0]
-    console.log('file',file)
     const filename = file.name
     const size = file.size
     const type = file.type
     const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
     const fileparts = filename.split('.')
-    const extension = fileparts[fileparts.lenght-1 ]
-    const presignedurl = await s3uploadkey(cognito_user_uuid)
+    const extension = fileparts[fileparts.length-1]
+    const presignedurl = await s3uploadkey(extension)
     console.log('Undefined Error',presignedurl)
 
     try {
@@ -72,7 +66,7 @@ export default function ProfileForm(props) {
       }})
       let data = await res.json();
       if (res.status === 200) {
-        setPresignedurl(data.url)
+        
       } else {
         console.log(res)
       }
