fd2297dccb2d6cdb828ceb3c749acd2d6cda2ca0
DavidTausend <davidtausend-911@hotmail.com>
2023-04-28 12:14:35 +0000

    Fixing problem with backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -110,22 +110,22 @@ cors = CORS(
 
 
 # rollbar
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
+#rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+#@app.before_first_request
+#def init_rollbar():
+    #"""init rollbar module"""
+    #rollbar.init(
         # access token
-        rollbar_access_token,
+        #rollbar_access_token,
         # environment name
-        'production',
+        #'production',
         # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
+        #root=os.path.dirname(os.path.realpath(__file__)),
         # flask already sets up logging
-        allow_logging_basic_config=False)
+        #allow_logging_basic_config=False)
 
     # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+    #got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route('/api/health-check')
 def health_check():
