f65eb25c62a0c9d367076eb7ad266b76d62c22b8
DavidTausend <davidtausend-911@hotmail.com>
2023-05-23 11:39:03 +0000

    debugging frontend replies 2

--- backend-flask/db/sql/activities/home.sql
+++ backend-flask/db/sql/activities/home.sql
@@ -1,28 +1,28 @@
 SELECT
-  replies.uuid,
-  reply_users.display_name,
-  reply_users.handle,
-  replies.message,
-  replies.replies_count,
-  replies.reposts_count,
-  replies.likes_count,
-  replies.reply_to_activity_uuid,
-  replies.created_at
+  activities.uuid,
+  users.display_name,
+  users.handle,
+  activities.message,
+  activities.replies_count,
+  activities.reposts_count,
+  activities.likes_count,
+  activities.expires_at,
+  activities.created_at,
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
   SELECT
-    activities.uuid,
-    users.display_name,
-    users.handle,
-    activities.message,
-    activities.replies_count,
-    activities.reposts_count,
-    activities.likes_count,
-    activities.reply_to_activity_uuid,
-    activities.created_at
+    replies.uuid,
+    reply_users.display_name,
+    reply_users.handle,
+    replies.message,
+    replies.replies_count,
+    replies.reposts_count,
+    replies.likes_count,
+    replies.reply_to_activity_uuid,
+    replies.created_at
   FROM public.activities replies
-  WHERE 
+  LEFT JOIN public.users reply_users ON reply_users.uuid = replies.user_uuid
+  WHERE
     replies.reply_to_activity_uuid = activities.uuid
-  LEFT JOIN public.users ON reply_users.uuid = replies.user_uuid
   ORDER BY activities.created_at ASC
   ) array_row) as replies
 FROM public.activities
--- backend-flask/services/create_reply.py
+++ backend-flask/services/create_reply.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, timezone
 from lib.db import db
 
 class CreateReply:
-  def run(message, user_handle, activity_uuid):
+  def run(message, cognito_user_id, activity_uuid):
     model = {
       'errors': None,
       'data': None
@@ -33,14 +33,14 @@ class CreateReply:
       model['data'] = object_json
     return model
 
-    def create_reply(cognito_user_id, activity_uuid, message):
+  def create_reply(cognito_user_id, activity_uuid, message):
       sql = db.template('activities','reply')
       uuid = db.query_commit(sql,{
         'cognito_user_id': cognito_user_id,
         'reply_to_activity_uuid': activity_id,
         'message': message,
     })
-    return uuid
+  return uuid
   def query_object_activity(uuid):
     sql = db.template('activities','object')
     return db.query_object_json(sql,{
