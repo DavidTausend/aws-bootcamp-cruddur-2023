7b77e706dd3bd6aeb0057b1f24bb4546efc700c4
DavidTausend <davidtausend-911@hotmail.com>
2023-05-26 05:01:23 +0000

    wip activity show page

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -26,13 +26,13 @@ export default function ReplyForm(props) {
     }
     post(url,payload_data,setErrors,function(data){
       // add activity to the feed
-      let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
-      let found_activity = activities_deep_copy.find(function (element) {
-        return element.uuid ===  props.activity.uuid;
-      });
-      found_activity.replies.push(data)
+      //let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
+      //let found_activity = activities_deep_copy.find(function (element) {
+        //return element.uuid ===  props.activity.uuid;
+      //});
+      //found_activity.replies.push(data)
 
-      props.setActivities(activities_deep_copy);
+      //props.setActivities(activities_deep_copy);
       // reset and close the form
       setCount(0)
       setMessage('')
---
+++ frontend-react-js/src/pages/ActivityShowPage.js
@@ -0,0 +1,87 @@
+import './ActivityShow.css';
+import React from "react";
+//import { trace } from '@opentelemetry/api';
+
+import DesktopNavigation  from 'components/DesktopNavigation';
+import DesktopSidebar     from 'components/DesktopSidebar';
+import ActivityForm from 'components/ActivityForm';
+import ReplyForm from 'components/ReplyForm';
+import {get} from 'lib/Requests';
+import {checkAuth} from 'lib/CheckAuth';
+
+export default function ActivityShowPage() {
+  const [replies, setReplies] = React.useState([]);
+  const [popped, setPopped] = React.useState(false);
+  const [poppedReply, setPoppedReply] = React.useState(false);
+  const [replyActivity, setReplyActivity] = React.useState({});
+  const [user, setUser] = React.useState(null);
+  const dataFetchedRef = React.useRef(false);
+  //const tracer = trace.getTracer();
+
+  const loadData = async () => {
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+    get(url,null,function(data){
+      setActivities(data)
+    })
+  }
+  //React.useEffect(() => {
+  //  if (dataFetchedRef.current) return;
+  //  dataFetchedRef.current = true;
+  
+ //   tracer.startActiveSpan('HomeFeedPage', (span) => {
+ //     tracer.startActiveSpan('load_data', (span) => {
+ //       span.setAttribute('endpoint', '/api/activities/home');
+ //       loadData();
+ //       span.end()
+ //     })
+//      tracer.startActiveSpan('check_auth', (span) => {
+ //       span.setAttribute('endpoint', '/api/auth');
+ //       checkAuth(setUser);
+  //      span.end()
+  //    })
+  //    span.end()
+  //  })
+ // }, [])
+  
+
+  React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    loadData();
+    checkAuth(setUser);
+  }, [])
+
+  return (
+    <article>
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <div className='content'>
+        <ActivityForm  
+          popped={popped}
+          setPopped={setPopped} 
+          setActivities={setActivities} 
+        />
+        <ReplyForm 
+          activity={replyActivity} 
+          popped={poppedReply} 
+          setPopped={setPoppedReply} 
+          setActivities={setActivities} 
+          activities={activities} 
+        />
+        <div className='activity_feed'>
+          <div className='activity_feed_heading'>
+            <div className='title'>Home</div>
+          </div> 
+          <RepliesFeed 
+            title="Home" 
+            setReplyActivity={setReplyActivity} 
+            setPopped={setPoppedReply} 
+            activities={activities} 
+          />
+          </div>
+        </div>
+        <DesktopSidebar user={user} />
+    </article>
+  );
+}
